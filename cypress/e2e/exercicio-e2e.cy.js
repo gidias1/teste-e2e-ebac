139a44632d0ff600de7bfaf1536925ca9c699143
gidias1 <giovanna2001.dias@gmail.com>
2025-04-30 11:45:34 -0300

    Subindo alterações no exercicio

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
       Para fazer um pedido de 4 produtos 
@@ -9,14 +8,46 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       Preenchendo todas opções no checkout
       E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+      describe('Fluxo de compra ponta a ponta na Loja EBAC', () => {
+        it('Deve fazer um pedido de 4 produtos com sucesso', () => {
+            // Login
+            cy.login('giovanna2001.teste@gmail.com', '123456');
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
-
-
-})
+            cy.limparCarrinho();
+    
+            // Adicionar primeiro produto
+            cy.adicionarProduto('.post-2559 > .product-block > .block-inner > .image > .product-image > .image-hover', 'XS', 'Green');
+    
+            // Adicionar segundo produto
+            cy.adicionarProduto('.post-3374 > .product-block > .block-inner > .image > .product-image > .image-hover', '32', 'Black');
+    
+            // Adicionar terceiro produto
+            cy.adicionarProduto('.post-3647 > .product-block > .block-inner > .image > .product-image > .image-hover', 'S', 'Gray');
+    
+            // Adicionar quarto produto
+            cy.adicionarProduto('.post-3964 > .product-block > .block-inner > .image > .product-image > .image-hover', 'XS', 'Purple');
+    
+            // Ir para o carrinho
+            cy.get('.dropdown-toggle > .mini-cart-items').click();
+    
+            // Ir para o checkout
+            cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click();
+    
+            // checkout
+            cy.get('#billing_first_name').clear().type('Giovanna');
+            cy.get('#billing_last_name').clear().type('Teste');
+            cy.get('#billing_address_1').clear().type('Rua das Flores, 123');
+            cy.get('#billing_city').clear().type('São Paulo');
+            cy.get('#billing_postcode').clear().type('12345000');
+            cy.get('#billing_phone').clear().type('11999999999');
+            cy.get('#billing_email').clear().type('giovanna2001.teste@gmail.com');
+    
+            // Finalizar pedido
+            cy.get('#terms').click();
+            cy.get('#place_order').click();
+    
+            // Validar pedido concluído
+            cy.get('.woocommerce-notice').should('contain', 'Obrigado');
+        });
+    });
+    
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,35 @@
 Cypress.Commands.add('login', (usuario, senha) => {
+    cy.visit('/minha-conta')
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha)
     cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
 });
 
+Cypress.Commands.add('adicionarProduto', (seletorProduto, tamanho, cor) => {
+    cy.visit('/produtos')
+    cy.get(seletorProduto).click()
+    cy.get(`.button-variable-item-${tamanho}`).click()
+    cy.get(`.button-variable-item-${cor}`).click()
+    cy.get('.single_add_to_cart_button').click()
+});
+
+Cypress.Commands.add('limparCarrinho', () => {
+    cy.visit('/carrinho');
+  
+    cy.get('body').then($body => {
+      if ($body.find('.remove').length > 0) {
+        // Se tiver item no carrinho, removemos
+        cy.get('.remove').each(() => {
+          cy.get('.remove').first().click({ force: true });
+          cy.wait(500); // Aguarda um pouco para garantir a remoção
+        });
+  
+        // Garantir que o carrinho está vazio agora
+        cy.get('body').then($body2 => {
+          // Verifica se não há mais botão .remove (pois o carrinho está vazio)
+          expect($body2.find('.remove').length).to.eq(0);
+        });
+      }
+    });
+  });
